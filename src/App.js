d471d7b2f7a8f83bffb3fe06e385a3dcef4a061a
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-28 14:56:19 +0300

    Add Body_1 component to App for enhanced content display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/NavBar';
+import Body_1 from './components/Body_1';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Body_1 />
     </div>
   );
 }
