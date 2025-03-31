326ad373d2a513eba5cab55cd5844dcf96833698
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-31 18:57:55 +0300

    adding content to the main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/NavBar';
-import Body_1 from './components/Body_1';
+import TypeWriterr from './components/TypeWriter';
+import vanshmain from "./images/vansh-main.jpg"
+import github1 from "./images/github.png"
+import linkedin1 from "./images/linkedin.png"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Body_1 />
+      <div className="main-section">
+        <div className="profile">
+            <div className="image">
+              <img src={vanshmain} className="vanshmain" alt="vanshmain" />
+            </div>
+            <div className="info">
+                <p>Hello, I'm</p>
+                <p>Vansh Patel</p>
+                <TypeWriterr />
+                <img src={github1} className="github1" alt="github1" />
+                <img src={linkedin1} className="linkedin1" alt="linkedin1" />
+            </div>
+        </div>
+
+      </div>
     </div>
   );
 }
