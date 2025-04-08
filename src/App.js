44e8cb3d23ffb2ae503fd07b0021f731713a9700
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-08 20:10:20 +0300

    add about section with personal information and image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/NavBar';
 import TypeWriterr from './components/TypeWriter';
 import vanshmain from "./images/vansh-main.jpg"
+import aboutvansh from "./images/about-vansh.jpg"
 import github1 from "./images/github.png"
 import linkedin1 from "./images/linkedin.png"
 
@@ -24,7 +25,16 @@ function App() {
                 </div>
             </div>
         </div>
-
+        <hr class="faded-hr" />
+        <div className='about-section'>
+             <div className='moreinfo'>
+                <h1>ABOUT ME</h1>
+            </div>
+            <div className="facts">
+              <img src={aboutvansh} className="aboutvansh" alt="aboutvansh" />
+              <p>I'm a full-stack developer and graphic designer with a passion for creating dynamic web applications and stunning visual designs. I specialize in building modern frontends with React and Tailwind CSS, and backend systems with Node.js and Python. As a graphic designer, I focus on crafting intuitive user interfaces and visually appealing assets. My goal is to merge beautiful design with seamless functionality to create impactful user experiences. Outside of coding, I'm constantly exploring new design techniques and staying updated with the latest design trends.</p>
+            </div>
+        </div>
       </div>
     </div>
   );
