54c8e9fcd017d2bd8ea00c2a86dd19484def2d3a
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-09 16:11:58 +0300

    feat: add Skills component to enhance user profile section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import vanshmain from "./images/vansh-main.jpg";
 import aboutvansh from "./images/about-vansh.jpg";
 import github1 from "./images/github.png";
 import linkedin1 from "./images/linkedin.png";
+import Skills from './components/Skills';
 
 function App() {
   const [showSplash, setShowSplash] = useState(true);
@@ -42,6 +43,7 @@ function App() {
                 <p>I'm a full-stack developer and graphic designer with a passion for creating dynamic web applications and stunning visual designs. I specialize in building modern frontends with React and Tailwind CSS, and backend systems with Node.js and Python. As a graphic designer, I focus on crafting intuitive user interfaces and visually appealing assets. My goal is to merge beautiful design with seamless functionality to create impactful user experiences. Outside of coding, I'm constantly exploring new design techniques and staying updated with the latest design trends.</p>
                 <img src={aboutvansh} className="aboutvansh" alt="aboutvansh" />
               </div>
+              <Skills />
             </div>
           </div>
         </>
