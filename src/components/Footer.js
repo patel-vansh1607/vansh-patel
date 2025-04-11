508ecb89658477cf980b3df8b87dd199677f99fd
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-11 22:15:47 +0300

    making the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,26 @@
-const FooterComponent = () => {
-    return(
-        <div>
-            
+import React from 'react';
+import '../styles/Footer.css'; 
+import { FaGithub, FaLinkedinIn, FaInstagram, FaEnvelope } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-left">
+          Designed and Developed by <span>Vansh Patel</span>
         </div>
-    )
-}
+        <div className="footer-center">
+          © {new Date().getFullYear()} VP
+        </div>
+        <div className="footer-right">
+          <a href="mailto:pvansh830@gmail.com" target="_blank" rel="noopener noreferrer"><FaEnvelope /></a>
+          <a href="https://www.linkedin.com/in/vansh-patel-72650b26a/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="https://github.com/patel-vansh1607" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+          <a href="https://www.linkedin.com/in/vansh-patel-72650b26a/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
