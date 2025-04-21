cb52993d8fb6dad3d6c6c5b707515a7411c041cb
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-21 18:48:55 +0300

    adding all the 4 divs

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
+import Navbar from "../components/NavBar"
 import "../styles/About.css"
 
 const AboutPage = () => {
     return(
-        <div>
-            <h1>About Page</h1>
+        <div className="main div">
+            <Navbar />
+            <div className="div-1"></div>
+            <div className="div-2"></div>
+            <div className="div-3"></div>
+            <div className="div-4"></div>
         </div>
     )
 }
