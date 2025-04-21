6cc36e36e1195287e71342190bfa9a172660e8e1
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-21 20:18:51 +0300

    adding skills in div1

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,10 @@ const AboutPage = () => {
     return(
         <div className="main div">
             <Navbar />
-            <div className="div-1"></div>
+            <div className="div-1">
+                <p>designer</p>
+                <p>UI/UX | Branding | Graphic Design</p>
+            </div>
             <div className="div-2"></div>
             <div className="div-3"></div>
             <div className="div-4"></div>
